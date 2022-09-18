bfdc26a063f44a74e28d3403f2c8bdfeb6f37d1d
the-arcade-01 <aashisharcade@gmail.com>
2022-09-18 17:44:13 +0530

    showcase posts

--- blog/components/Header.tsx
+++ blog/components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function Header() {
   return (
-    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+    <header className="flex justify-between p-5 max-w-6xl mx-auto">
       <div className="flex items-center space-x-5">
         <Link href="/">
           <img

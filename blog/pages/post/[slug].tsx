7b005e931e6075a607efab8892008b4b1de1cbed
the-arcade-01 <aashisharcade@gmail.com>
2022-09-18 19:46:53 +0530

    issr added don't know wtf is it

---
+++ blog/pages/post/[slug].tsx
@@ -0,0 +1,108 @@
+import { sanityClient, urlFor } from "../../sanity";
+import { Post } from "../../typings";
+
+import Header from "../../components/Header";
+import { GetStaticProps } from "next";
+import PortableText from "react-portable-text";
+
+interface Props {
+  post: Post;
+}
+
+export default function PostIndividual({ post }: Props) {
+  return (
+    <main>
+      <Header />
+      <img
+        className="w-full h-40 object-cover"
+        src={urlFor(post.mainImage).url()}
+        alt=""
+      />
+
+      <article className="max-w-3xl mx-auto p-5">
+        <h1 className="text-3xl mt-10 mb-3 mx-auto">{post.title}</h1>
+        <h2 className="text-xl font-light text-gray-500 mb-2">
+          {post.description}
+        </h2>
+
+        <div className="flex items-center space-x-2">
+          <img
+            className="h-10 w-10 rounded-full"
+            src={urlFor(post.author.image).url()!}
+            alt=""
+          />
+          <p className="font-extralight text-sm">
+            Blog post by{" "}
+            <span className="text-green-600">{post.author.name}</span> -
+            Published at {new Date(post._createdAt).toLocaleString()}
+          </p>
+        </div>
+
+        <div>
+          <PortableText
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+            content={post.body}
+            serializers={
+                
+            }
+          />
+        </div>
+      </article>
+    </main>
+  );
+}
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "post"]{
+        _id,
+        slug {
+            current
+        }
+    }`;
+
+  const posts = await sanityClient.fetch(query);
+
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `*[_type == "post" && slug.current == $slug][0]{
+        _id,
+        _createdAt,
+        title,
+        author-> {
+            name,
+            image
+        },
+        description,
+        mainImage,
+        slug,
+        body
+    }`;
+  const post = await sanityClient.fetch(query, {
+    slug: params?.slug,
+  });
+
+  if (!post) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 60, // after 60 seconds, it'll update the new cache
+  };
+};

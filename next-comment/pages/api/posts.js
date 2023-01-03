dea37dea8087609c0c706a107aa8bd7bf5bf9957
Aashish Koshti <aashisharcade@gmail.com>
2023-01-03 19:19:02 +0530

    next-comment connect prisma on frontend

---
+++ next-comment/pages/api/posts.js
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  return await prisma.user.findMany({
+    select: {
+      id: true,
+      name: true,
+    },
+  });
+}
